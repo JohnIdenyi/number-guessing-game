44eaaa1c7fa9d2fa38037ee0502586f1f7d80ba8
John Idenyi <60501639+JohnIdenyi@users.noreply.github.com>
2022-06-02 23:55:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const username = prompt("Please input your username");
+let min = 1;
+let max = 2;
+let stage = 1;
+let points = 0;
+const guessNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+const range = Number(prompt(`Hi ${username}! Enter your guess`));
+
+// Check if guess number is correct or not
+function guess(range, guessNumber) {
+    if (range < min || range > max || isNaN(range)) {
+        console.log(`Number must between ${min} and ${max}`);
+    } else {
+        if (range === guessNumber) {
+            console.log("Correct");
+            nextStage();
+        } else {
+            console.log("Game over");
+            console.log(`Total points: ${points}`);
+        }
+    }
+
+}
+
+guess(range, guessNumber);
+
+// next Stage function to be executed if guess is correct
+function nextStage() {
+    // Add 1 to max range
+    max++;
+    // Increase stage by 1
+    stage++
+    // Increase points by 1 and output points 
+    points++
+    points > 1 ? console.log(`${points} points`) : console.log(`${points} point`);
+    // Generate new random number 
+    let guessNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+
+    // Prompt user to enter new number
+    console.log(`Stage ${stage}! Range has been increased from ${min} - ${max}`);
+    let range = Number(prompt(`${username} Enter your guess`));
+
+    // call guess function to check new guess
+    guess(range, guessNumber);
+}
+
